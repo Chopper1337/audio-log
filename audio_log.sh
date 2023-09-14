a228684a5dd9213ee6b688137e453f28ddff3c4e
Chopper <43015696+Chopper1337@users.noreply.github.com>
2023-09-14 21:41:30 +0100

    Add checks, finish main script basic

--- audio_log.sh
+++ audio_log.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
-today=$(date +%Y%m%d_%H%M%S)
-mkdir -p $today
-cd $today || exit
+# Take the first arg as the name of the model
+# If no arg is given, use ggml-small.bin as default
+model=${1:-ggml-small.bin}
 
-arecord -f cd -t wav "$today.wav"
+check_whisper()
+{
+    # Check if whisper.cpp/main exists
+    if [ ! -f whisper.cpp/main ]; then
+        echo "whisper.cpp/main not found!"
+        echo "Please build whisper.cpp first by running setup.sh or manually following whisper.cpp/README.md"
+        exit 1
+    fi
+
+    # Check if the model exists
+    if [ ! -f "whisper.cpp/models/$model" ]; then
+        echo "$model not found, please run setup.sh first or manually download the model following whisper.cpp/README.md"
+        exit 1
+    fi
+}
+
+check_whisper
+today=$(date +%Y%m%d_%H%M%S)
+echo "Today is $today"
+echo "Creating directory $today"
+mkdir -p "$today"
+cd "$today" || exit
+echo Recording started, press Ctrl+C to stop
+arecord -f cd -r 16000 -t wav "$today.wav"
+../whisper.cpp/main -m ../whisper.cpp/models/ggml-small.bin -f "$today.wav" --output-txt
+cd ..
+echo "Transcript saved to $today/$today.txt"
+echo "Audio saved to $today/$today.wav"
--- setup.sh
+++ setup.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+echo "Building whisper.cpp..."
+
 # Navigate into the whisper.cpp directory
 cd ./whisper.cpp/ || exit
 
 # Build whisper.cpp
 make
+
+# Ask the user if the want to download the small model
+read -p "Do you want to download the small model? (Recommended for basic use) [Y/n] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+    echo "Please run whisper.cpp/download-ggml-model.sh manually."
+    exit 0
+fi
 
 # Navigate to the models directory
 cd ./models/ || exit
